2ffa58211728f8e5b8be9a0c7b49c9096dbbfa7e
AndreansxTech <koliberekart@icloud.com>
2025-05-08 00:28:31 +0200

    mail address also not hardcoded

--- mailing-client/main.py
+++ mailing-client/main.py
@@ -1,4 +1,10 @@
 import smtplib
 
+server = smtplib.SMTP("smtp.gmail.com", 25)
+server.ehlo()
+
 with open("password.txt", "r") as passfile:
     password = passfile.read()
+with open("mailaddr.txt", "r") as mailfile:
+    mailaddr = mailfile.read()
+server.login(mailaddr, password)
